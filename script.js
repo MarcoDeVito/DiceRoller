8f93c5ef693d1c745151f7cffb70eff4dea62a6a
Marco De Vito <marco.devito93@gmail.com>
2024-10-16 15:22:51 +0200

    formule riodinabili

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ svantaggiobtnclick = () => {
         vantaggiobtn.checked = false
     }
 }
+
 document.addEventListener('DOMContentLoaded', () => {
     InsertSkill()
     loadStats()
@@ -26,6 +27,9 @@ document.addEventListener('DOMContentLoaded', () => {
     loadRollHistory();
     loadModificators()
     loadSelectedSkills();
+    loadScrollable();
+
+
     vantaggiobtn = document.querySelector('#vantaggio')
     svantaggiobtn = document.querySelector('#svantaggio')
 
@@ -40,6 +44,86 @@ document.addEventListener('keydown', event => {
     }
 });
 
+function loadScrollable(){
+    const sortableList = document.querySelector("#savedFormulas");
+    const items = sortableList.querySelectorAll(".item");
+
+    items.forEach(item => {
+        const handle = item.querySelector(".bars");
+
+        if (handle) {
+            handle.addEventListener("touchstart", (e) => {
+                e.stopPropagation();
+                e.preventDefault()
+                // item.setAttribute("draggable", "true");
+                item.classList.add("dragging")
+            });
+
+            handle.addEventListener("touchend", () => {
+                item.classList.remove("dragging");
+                // item.removeAttribute("draggable");
+
+                // Salva l'ordine attuale dopo il riordinamento
+                saveCurrentOrder();
+            });
+        }
+
+        // Assicurarsi che i pulsanti restino cliccabili
+        const buttons = item.querySelectorAll("button");
+        buttons.forEach(button => {
+            button.addEventListener("click", (e) => {
+                e.stopPropagation();
+            });
+        });
+    });
+
+    const initSortableList = (e) => {
+        e.preventDefault();
+        const draggingItem = document.querySelector(".dragging");
+        let siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
+        
+        
+        let clientY = e.touches[0].clientY;
+        console.log(clientY);
+        
+        
+        let nextSibling = siblings.find(sibling => {
+            const siblingRect = sibling.getBoundingClientRect();
+            aaa=(siblingRect.top + siblingRect.height)
+            console.log("aaa:"+aaa);
+            
+            return clientY <= aaa;
+        });
+        if(nextSibling)
+        {
+            sortableList.insertBefore(draggingItem, nextSibling);
+
+        }
+
+    }
+
+    sortableList.addEventListener("touchmove", initSortableList);
+    sortableList.addEventListener("touchstart", e => e.preventDefault());
+}
+
+
+// Funzione per salvare l'ordine attuale aggiornando gli indici nel localStorage
+function saveCurrentOrder() {
+    const items = document.querySelectorAll("#savedFormulas .item");
+    const savedFormulas = JSON.parse(localStorage.getItem("formulas")) || [];
+
+    // Aggiorna gli indici in base alla posizione corrente degli elementi
+    items.forEach((item, index) => {
+        const formulaId = item.querySelector("button").dataset.id;
+        const formula = savedFormulas.find(f => f.id === formulaId);
+        if (formula) {
+            formula.index = index; // Assegna il nuovo indice
+        }
+    });
+
+    // Salva nuovamente le formule con i nuovi indici
+    localStorage.setItem("formulas", JSON.stringify(savedFormulas));
+}
 
 
 function appendToDisplay(value, isDice) {
@@ -224,10 +308,12 @@ function loadRollHistory() {
 }
 
 
-function saveFormulaToLocalStorage(formula, localStorageName = "formulas") {
-    let formulas = JSON.parse(localStorage.getItem(localStorageName)) || [];
-    formulas.push(formula);
-    localStorage.setItem(localStorageName, JSON.stringify(formulas));
+// Funzione per salvare una nuova formula assegnando l'indice
+function saveFormulaToLocalStorage(formula) {
+    const savedFormulas = JSON.parse(localStorage.getItem("formulas")) || [];
+    formula.index = savedFormulas.length; // Imposta l'indice come l'ultimo
+    savedFormulas.push(formula);
+    localStorage.setItem("formulas", JSON.stringify(savedFormulas));
 }
 
 function removeFormulaFromLocalStorage(name) {
@@ -238,24 +324,34 @@ function removeFormulaFromLocalStorage(name) {
 
 function createFormulaElement(id, name, save = "savedFormulas") {
     const savedFormulasElement = document.getElementById(save);
+    
 
-
-    listItem = document.createElement('li');
-    listItem.className = 'list-group-item w-100 d-flex align-items-center ';
+    listItem = document.createElement('div');
+    listItem.className = 'mb-2 w-100 d-flex align-items-center item';
+    
 
     const btnGroup = document.createElement('div')
     btnGroup.className = 'btn-group w-100'
+   
 
+    const draggable= document.createElement("span")
+    draggable.className="btn bars btn-custom flex-shrink-1 d-flex justify-content-center align-items-center"
+    draggable.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 448 512"><path fill="#FFF" d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>`
 
     const formulaButton = document.createElement('button');
     formulaButton.className = 'btn btn-secondary';
-    formulaButton.style = "width:70%; overflow:hidden"
+    formulaButton.style = "width:60%; overflow:hidden"
     formulaButton.dataset.id = id;
     formulaButton.textContent = name;
 
-
-
-
+    formulaButton.addEventListener('touchend', function (event) {
+        event.stopPropagation();
+        textsplit = this.innerText.split(":")
+        formulaname = textsplit[0]
+        document.getElementById('display').value = this.dataset.id;
+        rollDice(false, formulaname);
+        lastButtonWasDice = true;
+    });
     formulaButton.addEventListener('click', function (event) {
         event.stopPropagation();
         textsplit = this.innerText.split(":")
@@ -268,36 +364,45 @@ function createFormulaElement(id, name, save = "savedFormulas") {
     const editButton = document.createElement('button');
     editButton.className = 'btn btn-custom flex-shrink-1';
     editButton.innerHTML = '✏️';
-    editButton.onclick = () => {
+    let editClick=()=>{
         showEditModal(id, name, listItem);
-    };
+    }
+    editButton.addEventListener('touchend', editClick);
+    editButton.addEventListener('click', editClick);
 
     const deleteButton = document.createElement('button');
     deleteButton.className = 'btn btn-danger flex-shrink-1';
     deleteButton.innerHTML = '🗑️';
-    deleteButton.addEventListener('click', function () {
+   let removeClick=()=>{ 
         savedFormulasElement.removeChild(listItem);
         removeFormulaFromLocalStorage(name);
-
-        loadFormulas()
-    });
+        loadFormulas()}
+    deleteButton.addEventListener('touchend', removeClick);
+    deleteButton.addEventListener('click', removeClick);
 
     listItem.innerHTML = '';
+    btnGroup.appendChild(draggable);
     btnGroup.appendChild(formulaButton);
     btnGroup.appendChild(editButton);
     btnGroup.appendChild(deleteButton);
     listItem.appendChild(btnGroup)
     savedFormulasElement.appendChild(listItem);
+    
+    
+    
+  
 
 }
 
 function saveFormula() {
     const display = document.getElementById('display').value;
+    const savedFormulas = JSON.parse(localStorage.getItem("formulas")) || [];
     let name = prompt("Inserisci un nome per la formula:");
     if (display && name) {
         name += ": " + display;
         saveFormulaToLocalStorage({ id: display, name });
         loadFormulas()
+        loadScrollable();
 
     } else {
         alert("Non è possibile salvare una formula vuota o senza nome.");
@@ -306,18 +411,19 @@ function saveFormula() {
 
 function loadFormulas(localStorageName = "formulas", save = 'savedFormulas') {
     const savedFormulas = JSON.parse(localStorage.getItem(localStorageName)) || [];
-    const tutorialFormulas = document.getElementById(save)
+    const tutorialFormulas = document.getElementById(save);
     tutorialFormulas.innerHTML = '';
 
     if (savedFormulas.length === 0) {
+        tutorialFormulas.innerHTML = '<p>Ancora nessuna formula salvata</p><p>Premi ❤ per salvare una formula tra i preferiti</p>';
+    } else {
+        // Ordina le formule in base all'indice salvato
+        const orderedFormulas = savedFormulas
+            .sort((a, b) => (a.index ?? 0) - (b.index ?? 0)); // Usa `??` per gestire vecchi dati senza indice
 
-        tutorialFormulas.innerHTML = '<p>Ancora nessuna formula salvata</p><p>Premi ❤ per salvare una formula tra i preferiti</p>'
-    }
-    else {
-        savedFormulas.forEach(formula => {
+        orderedFormulas.forEach(formula => {
             createFormulaElement(formula.id, formula.name);
         });
     }
-
 }
 
